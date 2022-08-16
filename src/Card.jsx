8f41d4c4e23122cfbe2966e19fb3dcd96fb564f9
DomenJ2003 <domen.jakopic2@gmail.com>
2022-08-16 13:09:22 +0200

    osnovni izgled kartice

--- src/Card.jsx
+++ src/Card.jsx
@@ -9,13 +9,22 @@ function Card() {
       <div className="okvirSlike">
         <img className='slika' src="../public/bg-img.jpg" alt="" />
       </div>
+      <div className="profil"></div>
       <div className="splosni-podatki">
-        
+        <h3>Ime Primek</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, odio!</p>
       </div>
       <div className="links">
-        
+        <a href="#">
+          Hungary
+        </a>
+        <a href="#">
+          Email me
+        </a>
       </div>
-      <div className="follow"></div>
+      <div className="follow">
+        Fo
+      </div>
     </div>
   )
 }
