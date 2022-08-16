85dd2125145bc8e1ea381e3f1797a89d6338b4d5
DomenJ2003 <domen.jakopic2@gmail.com>
2022-08-16 12:36:54 +0200

    začetek delanja kartice

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,11 +2,25 @@ import { useState } from 'react'
 import './Card.css'
 
 function Card() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="Card">
-      
+      <div className="okvirSlike">
+        <img className='slika' src="../public/bg-img.jpg" alt="" />
+      </div>
+      <div className="splosni-podatki">
+        <h3>Ana Lovefully</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, quis!</p>
+      </div>
+      <div className="links">
+        <div className="country">
+          Hungary
+        </div>
+        <div className="mail">
+          Send email
+        </div>
+      </div>
     </div>
   )
 }
