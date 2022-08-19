014d40e272523ab54c114ed07d3188a5d9bb0eb2
DomenJ2003 <domen.jakopic2@gmail.com>
2022-08-19 11:42:18 +0200

    home page design

---
+++ src/Components/General/Card.jsx
@@ -0,0 +1,27 @@
+
+import { useState } from 'react'
+import './Card.css'
+
+function Card(props) {
+  
+  return (
+    <div className="card-container">
+      <div className="card-header">
+        <span className='card-title'>
+            <i className="fa-solid fa-user"></i>{'  '}
+            {props.header}
+        </span>
+        <br />
+        <span>{props.date.toLocaleDateString()}</span>
+        
+      </div>
+      <div className="card-content">
+      {props.content}
+      <img src={props.image} className="card-image"/>
+      </div>
+   
+    </div>
+  );
+}
+
+export default  Card
--- src/Components/Sections/HomePageContent.jsx
+++ src/Components/Sections/HomePageContent.jsx
@@ -1,11 +1,65 @@
 import { useState } from 'react'
+import Card from '../General/Card';
 import './HomePageContent.css'
 
 function HomePageContent() {
+
+  const card_data = [
+    
+    {
+      'header': 'Prva karta',
+      'content': 'descriptons...',
+      'date': new Date(),
+      'image' : 'https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-02.jpg?w=640'
+    },
+    {
+      'header': 'Druga karta',
+      'content': 'descriptons...',
+      'date': new Date(),
+      'image' : 'https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-02.jpg?w=640'
+    },
+    {
+      'header': 'Tretija karta',
+      'content': 'descriptons...',
+      'date': new Date(),
+      'image' : 'https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-02.jpg?w=640'
+    },
+    {
+      'header': 'Četrta karta',
+      'content': 'descriptons...',
+      'date': new Date(),
+      'image' : 'https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-02.jpg?w=640'
+    },
+    {
+      'header': 'Peta karta',
+      'content': 'descriptons...',
+      'date': new Date(),
+      'image' : 'https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-02.jpg?w=640'
+    },
+    {
+      'header': 'Šesta karta',
+      'content': 'descriptons...',
+      'date': new Date(),
+      'image' : 'https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-02.jpg?w=640'
+    },
+    {
+      'header': 'Sedma karta',
+      'content': 'descriptons...',
+      'date': new Date(),
+      'image' : 'https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-02.jpg?w=640'
+    },
+  ]
+
+ 
   
   return (
     <div className="homepage-content">
-      <h1>Test</h1>
+      <h1>Recent Activity</h1>
+      <div className="cards-container">
+        {card_data.map((card_data, index)=>(
+          <Card key={index} {...card_data}/>
+        ))}
+      </div>
     </div>
   );
 }
