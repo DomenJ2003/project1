55cdb24179f441209ee07af0cb810143a5568907
DomenJ2003 <domen.jakopic2@gmail.com>
2022-08-21 14:07:56 +0200

    set random hero img

--- src/Components/Sections/HeroSection.jsx
+++ src/Components/Sections/HeroSection.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import './HeroSection.css'
+import { useState } from "react";
+import "./HeroSection.css";
 
 function HeroSection() {
-  
+  const getRndImg = () => {
+    const num = Math.floor(Math.random() * 7) + 1;
+    let s =
+      "linear-gradient(90deg, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0.1) 55%), url('../../../public/bg" +
+      num.toString() +
+      ".jpg')";
+    return s;
+  };
+
   return (
-    <div className="hero-section">
-      
-    </div>
+    <div
+      className="hero-section"
+      style={{
+        background: `${getRndImg()}`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    ></div>
   );
 }
 
-export default  HeroSection
+export default HeroSection;
