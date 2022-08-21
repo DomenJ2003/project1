a7d8f1b1f7470221bd5e0397792b154a0bbd6deb
DomenJ2003 <domen.jakopic2@gmail.com>
2022-08-21 13:44:02 +0200

    try responsive

--- src/Components/General/Dropodown.jsx
+++ src/Components/General/Dropodown.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import './dropdown.css'
+import { useState } from "react";
+import "./dropdown.css";
+import icon_category from "../../helper/icon-dict";
 
 function Dropdown(props) {
+  var links = [];
 
-    var links = []
-
-    for(const link in props.links){
-        
-        links.push(<a className='dropdown-link' href="#" key={link}>{props.links[link]}</a>)
-    }
-  
+  for (const link in props.links) {
+    links.push(
+      <a className="dropdown-link" href="#" key={link}>
+        <i className={icon_category[props.links[link]]}></i>
+        {"  " + props.links[link]}
+      </a>
+    );
+  }
 
   return (
     <div className="dropdown">
-                <div className="link">
-                    <span>
-                        {props.name}
-                    </span>
-                    <span>
-                        <svg width={24} height={24} className="arrow">
-                            <path d="M12 15.25a1 1 0 01-.7-.29l-4.58-4.5A1.011 1.011 0 018.12 9L12 12.85 15.88 9a1 1 0 111.4 1.42L12.7 15a1 1 0 01-.7.25z"></path>
-                        </svg>
-                    </span>
-                </div>
-                <div className="dropdown-content">
-                    {links}
-                </div>
-        </div>
-  )
+      <div className="link">
+        <span>{props.name}</span>
+        <span>
+          <svg width={24} height={24} className="arrow">
+            <path d="M12 15.25a1 1 0 01-.7-.29l-4.58-4.5A1.011 1.011 0 018.12 9L12 12.85 15.88 9a1 1 0 111.4 1.42L12.7 15a1 1 0 01-.7.25z"></path>
+          </svg>
+        </span>
+      </div>
+      <div className="dropdown-content">{links}</div>
+    </div>
+  );
 }
 
-export default Dropdown
+export default Dropdown;
