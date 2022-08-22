afb185d8029f1a266605c94fda0460c23da3e2fe
DomenJ2003 <domen.jakopic2@gmail.com>
2022-08-22 11:57:09 +0200

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,144 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const counter = () => {
-    setCount(count + 1);
-  };
 
   return (
     <div className="App">
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand">Navbar</a>
-          <form className="d-flex input-group w-auto">
-            <input
-              type="search"
-              className="form-control rounded"
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="search-addon"
-            />
-            <span className="input-group-text border-0" id="search-addon">
-              <i className="fas fa-search"></i>
-            </span>
-          </form>
-        </div>
-      </nav>
+      <Navbar />
       <div className="main-container">
-        <div className="row">
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <HomePage />
       </div>
     </div>
   );
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,118 @@
+
+function HomePage(){
+
+    return(
+        <div className="row">
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: "45px", height: "45px" }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: "45px", height: "45px" }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: "45px", height: "45px" }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: "45px", height: "45px" }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: "45px", height: "45px" }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-6 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
+                    alt=""
+                    style={{ width: "45px", height: "45px" }}
+                    className="rounded-circle"
+                  />
+                  <div className="ms-3">
+                    <p className="fw-bold mb-1">John Doe</p>
+                    <p className="text-muted mb-0">john.doe@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+}
+
+export default HomePage;
