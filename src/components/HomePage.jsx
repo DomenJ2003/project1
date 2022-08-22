708baa1fe55077f32245b0790951802fd370935f
DomenJ2003 <domen.jakopic2@gmail.com>
2022-08-22 12:52:11 +0200

    modal

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,118 +1,20 @@
+import Card from "./Card";
+import AddTodoModal from "./modals/AddTodoModal";
 
-function HomePage(){
+function HomePage() {
+  const todos = [1, 1, 1, 1, 1];
+  const isModalVisible = true
 
-    return(
-        <div className="row">
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-6 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="d-flex align-items-center">
-                  <img
-                    src={"https://mdbootstrap.com/img/new/avatars/8.jpg"}
-                    alt=""
-                    style={{ width: "45px", height: "45px" }}
-                    className="rounded-circle"
-                  />
-                  <div className="ms-3">
-                    <p className="fw-bold mb-1">John Doe</p>
-                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    );
+  return (
+    <>
+      <div className="row">
+        {todos.map((todo, index) => (
+          <Card key={index} />
+        ))}
+      </div>
+      <AddTodoModal visible={isModalVisible} />
+    </>
+  );
 }
 
 export default HomePage;
---
+++ src/components/modals/AddTodoModal.jsx
@@ -0,0 +1,60 @@
+import "./modal.css";
+
+function AddTodoModal({ visible }) {
+  if (!visible) {
+    return <></>;
+  }
+  return (
+    <div className="my-modal-container">
+      <div class="modal" tabindex="-1">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-mdb-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <div>
+              <label class="form-label" for="todo-title">
+                  Example label
+                </label>
+                <input type="text" id="todo-title" class="form-control" />
+                
+              </div>
+              <div>
+              <label class="form-label" for="todo-descripton">
+                  Message
+                </label>
+                <textarea
+                  class="form-control"
+                  id="todo-descripton"
+                  rows="4"
+                ></textarea>
+                
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-mdb-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodoModal;
